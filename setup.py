82f62467ce6e405bb3cae81484bbf810ecbad286
andenrx <andenrx@gmail.com>
2022-02-23 15:36:30 -0700

    Fix setup

--- setup.py
+++ setup.py
@@ -13,4 +13,4 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ahmedfgad/GeneticAlgorithmPython",
-    packages=setuptools.find_packages())
+)
